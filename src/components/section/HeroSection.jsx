216c44fe61fd8fea58e2ef2708b41aa9cfaed929
Saroj-The-Man <sarojbartaula2@gmail.com>
2024-10-28 19:14:26 +0100

    Update HeroSection.jsx
    Changed titleof saroj blog

--- src/components/section/HeroSection.jsx
+++ src/components/section/HeroSection.jsx
@@ -30,7 +30,7 @@ export const HeroSection = () => {
               I&apos;m Saroj <span className="text-accent1">Bartaula</span>
             </span>
           </h1>
-          <p className="mt-2 font-medium xl:text-lg">AI/ML Engineer</p>
+          <p className="mt-2 font-medium xl:text-lg">I live in the Milky Way galaxy</p>
           <p className="mt-4 max-w-sm text-balance text-sm text-secondary xl:text-base">
             I&apos;m on a journey to create my own story. Along the way,
             I&apos;ve tried various things and will continue to do so. My focus
